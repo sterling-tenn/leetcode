0c77a345c3ae68daebddc9a07f43c3e4873e5e4d
sterling-tenn <56081843+sterling-tenn@users.noreply.github.com>
2024-05-08 14:58:26 -0400

    Time: 69 ms (21.12%), Space: 29.7 MB (33.11%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reorderList(self, head):
+        """
+        :type head: ListNode
+        :rtype: None Do not return anything, modify head in-place instead.
+        """
+        stack = []
+        curr = head
+        
+        # Put all nodes in a stack so we can get reverse order
+        while curr:
+            stack.append(curr)
+            curr = curr.next
+            
+        curr = head
+        
+        for i in range(len(stack)/2):
+            node = stack.pop()
+            node.next = curr.next
+            curr.next = node
+            
+            curr = curr.next.next # Every other node
+            
+        curr.next = None # end the list (we never removed the inserted reordered nodes)
