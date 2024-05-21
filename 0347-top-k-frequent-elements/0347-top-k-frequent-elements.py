e450f842704a69fe48383a4efa18c9e3bc86021a
sterling-tenn <56081843+sterling-tenn@users.noreply.github.com>
2024-05-21 19:16:25 -0400

    Time: 70 ms (78.63%), Space: 15 MB (59.23%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        # count and keep track of occurrences
+        hashmap = {}
+        for num in nums:
+            hashmap[num] = hashmap.get(num, 0) + 1
+        
+        sorted_by_values = sorted(hashmap, key = lambda x : hashmap[x], reverse = True)
+        
+        # append k values to the result
+        res = []
+        for idx, num in enumerate(sorted_by_values):
+            if k == idx:
+                return res
+            res.append(num)
+        return res
