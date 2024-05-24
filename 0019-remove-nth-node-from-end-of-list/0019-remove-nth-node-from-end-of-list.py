935cb4e9eb7a331a5eecb29ff98ed289c36de89f
sterling-tenn <56081843+sterling-tenn@users.noreply.github.com>
2024-05-24 18:21:33 -0400

    Update 0019-remove-nth-node-from-end-of-list.py

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,38 +1,22 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def removeNthFromEnd(self, head, n):
-        """
-        :type head: ListNode
-        :type n: int
-        :rtype: ListNode
-        """
-        length = self.getLength(head)
-        cnt = 0
-        curr = head
-        prev = None
-        while curr is not None:
-            cnt += 1
-            if cnt == length - n + 1:
-                if prev is not None:
-                    prev.next = curr.next
-                else: # edge case if size of LL is 1
-                    head = curr.next
-                break
-                
-            prev = curr
-            curr = curr.next
-            
-        return head
-        
-    def getLength(self, head):
-        cnt = 0
-        curr = head
-        while curr is not None:
-            cnt += 1
-            curr = curr.next
-            
-        return cnt
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode()
+        dummy.next = head
+        first = second = dummy
+
+        # count (n+1)-th node from beginning (not end)
+        for _ in range(n + 1):
+            first = first.next
+
+        # when 'first' reaches the end, second will have traversed to (n+1)-th node from the end, ie one node BEFORE the n-th node from the end
+        while first:
+            first = first.next
+            second = second.next
+
+        second.next = second.next.next # skip/delete node
+        return dummy.next
