ffe950f08f09bfa5c8984e4bccce1b7889ad4aa2
sterling-tenn <56081843+sterling-tenn@users.noreply.github.com>
2024-07-10 11:47:16 -0400

    Time: 59 ms (64.99%), Space: 17.8 MB (26.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        hashmap = {} # keep track of the numbers we've seen and their indexes
-        
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
         for idx, num in enumerate(nums):
             diff = target - num
-            
-            # have we seen the required difference value before?
-            if diff in hashmap.keys():
-                return [idx, hashmap[diff]]
-            
-            hashmap[num] = idx # add the current number and index
+            if diff in hashmap:
+                return [hashmap[diff], idx]
+            hashmap[num] = idx
