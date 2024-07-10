195297fb3d4abf3f7a68794b8db370007d1ac036
sterling-tenn <56081843+sterling-tenn@users.noreply.github.com>
2024-07-10 11:44:58 -0400

    Time: 48 ms (63.22%), Space: 16.8 MB (76.69%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,15 +1,10 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        
-        # track number of letter occurrences in each string
         hash_s = {}
-        for char in s:
-            hash_s[char] = hash_s.get(char, 0) + 1
-        
         hash_t = {}
-        for char in t:
-            hash_t[char] = hash_t.get(char, 0) + 1
+        for c in s:
+            hash_s[c] = hash_s.get(c, 0) + 1
+        for c in t:
+            hash_t[c] = hash_t.get(c, 0) + 1
             
         return hash_s == hash_t
