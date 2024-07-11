6dcb7a93ad3854e5190ac8de27aa7421c69f1c04
sterling-tenn <56081843+sterling-tenn@users.noreply.github.com>
2024-07-10 21:05:14 -0400

    Time: 43 ms (40.77%), Space: 16.6 MB (55.67%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,18 +1,13 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        
-        # base case, only 1 permutation
         if len(nums) == 1:
             return [nums]
         
+        res = []
         for idx, num in enumerate(nums):
-            # decrease the size of nums and find the permutations
-            remaining = nums[:idx] + nums[idx + 1:]
-            perms = self.permute(remaining)
+            remaining = nums[:idx] + nums[idx+1:]
             
-            # attach those permutations associated to the number that was removed
+            perms = self.permute(remaining)
             for perm in perms:
                 res.append([num] + perm)
-                
-        return res # final result
+        return res
